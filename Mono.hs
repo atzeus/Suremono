cdabccec6fa9477b3414bc70b34ab43b4b40bd2a
Atze van der Ploeg <atzeus@gmail.com>
2015-06-02 16:18:31 +0200

    Remark

--- Mono.hs
+++ Mono.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE Rank2Types #-}
 module Mono where
 
--- Claim: using this interface (meaning excluding the constructors), there is no (sub)term we can form with
--- type (forall x. Mono s (f x)) where f is some type function. This implies that
--- there is cannot be a subterm of type (forall x. Mono x). This is the reason
--- that STRefs (and IORefs) are type-safe
+-- Claim: using this interface (meaning excluding the constructors),
+-- there is no term we can form, such that a subterm (other than undefined) has type 
+-- type (forall x. Mono s (f x)) where f is some type function. 
+
+-- This implies that there is cannot be a subterm of type
+-- (forall x. Mono x). This is the reason that STRefs (and IORefs) 
+-- are type-safe: references are monomorphic.
 
 data Mono s a = Mono -- constructor not exported
 
