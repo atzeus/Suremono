9a97fbf26960356c3db9256c9251e88b20679899
atzeus <ploeg@cwi.nl>
2015-06-03 09:02:43 +0200

    Update Mono.hs

--- Mono.hs
+++ Mono.hs
@@ -3,10 +3,8 @@ module Mono(Mono,MonoM, newMono, runMono) where
 
 -- Claim: using this interface (meaning excluding the constructors),
 -- there is no term we can form, such that a subterm (other than undefined) has type 
--- type (forall x. Mono s (f x)) where f is some type function. 
-
--- This implies that there is cannot be a subterm of type
--- (forall x. Mono x). This is the reason that STRefs (and IORefs) 
+-- type (forall x. Mono s x) where f is some type function. 
+-- This is the reason that STRefs (and IORefs) 
 -- are type-safe: references are monomorphic.
 
 -- Note that if the Mono constructor was exported
